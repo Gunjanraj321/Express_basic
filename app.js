fb71d0d7a7704eec0e4c5075f08733680af96502
Gunjan Kumar <gunjanraj321@gmail.com>
2023-11-02 10:45:08 +0530

    expand route and html page

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const http = require('http')
-
 const express = require('express')
 
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use((req,res,next)=>{
-    res.send('<h1> hello to node js </h1>')
-    console.log('in the middleware')
-    next();
-})
-app.use((req,res,next)=>{
-    //res.send({ "key1": "value1" })
-    console.log('in the another middleware')
-})
+app.use(bodyParser.urlencoded({extended : true}));
 
-// const server = http.createServer(app);
-// server.listen(3000)
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST">product<input type="text" name="title" >quantity<input type="number" name="quantity" ><button type="submit">Add Product</button></form>')
+});
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+});
+app.use('/',(req,res,next)=>{
+    res.send('<h1> hello to node js </h1>')
+});
 
 app.listen(3000)
